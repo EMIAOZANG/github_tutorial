e842ef9f7fc8175ab6bd09f4ad76a89ce006753c
EMIAOZANG <christopher.w.lu@gmail.com>
2015-11-27 14:29:30 -0500

    Initial Commit

---
+++ github_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  github_tutorial
+//
+//  Created by Lu Christopher on 11/27/15.
+//  Copyright © 2015 Lu Christopher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
